07bb24cda8d0013cfe159c401b0689c8d28c44c4
Sean JA <seanjia923@163.com>
2026-01-29 14:44:36 +0800

    socket(pc): pattern finished

---
+++ socket_pc.py
@@ -0,0 +1,33 @@
+import socket
+import time
+import json
+import random
+
+HOST = '10.66.10.13'
+PORT = 0 # don't know yet
+
+def main():
+    print(f" Sending fake angles to {HOST}:{PORT} ")
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # try:
+    while True:
+        fake_angles = [random.randint(0,180) for _ in range (33)]
+
+        message = json.dumps(fake_angles).encode('utf-8')
+        client.sendto(message, (HOST, PORT))
+
+        print(f" Sent: {fake_angles[:3]} ")
+        time.sleep(0.033)
+
+    #     except KeyboardInterrupt:
+    #   #user command processing
+    #         print("\n ctrl+c detected. Exiting...")
+        
+    # finally:
+    #   #cleaning up
+    #     client.close()
+    #     print(" Socket closed. Goodbye")
+
+if __name__ == "__main__":
+    main()
