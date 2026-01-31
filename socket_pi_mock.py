8dab138202559ed842131ed85cefac569249922f
Sean JA <seanjia923@163.com>
2026-01-31 16:09:39 +0800

    Renamed

---
+++ socket_pi_mock.py
@@ -0,0 +1,25 @@
+import socket
+import json
+from mock_driver import MockServoKit 
+
+kit1 = MockServoKit(address=0x40)
+kit2 = MockServoKit(address=0x41)
+kit3 = MockServoKit(address=0x42)
+kits = [kit1, kit2, kit3]
+
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server.bind(('0.0.0.0', 52183))# Replace 00000 with desired port number
+
+print("UDP server up and listening")
+
+while True:
+    data, addr = server.recvfrom(4096)
+    print(f"Received message from {addr}:{data}")
+    angles = json.loads(data.decode('utf-8'))
+
+    for i, angle in enumerate(angles):
+        board_id = i//16
+        channel_id = i%16
+
+        if board_id < 3:
+            kits[board_id].servo[channel_id].angle = angle
