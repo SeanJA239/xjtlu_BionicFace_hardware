0deeb092779b3bfbea76c2845dcbb0733a586729
Sean JA <seanjia923@163.com>
2026-01-31 15:20:25 +0800

    Socket(pi): code for real kit complete

--- socket_pi.py
+++ socket_pi.py
@@ -1,25 +1,83 @@
-import socket
+import socket 
 import json
-from mock_driver import MockServoKit 
+import time
 
-kit1 = MockServoKit(address=0x40)
-kit2 = MockServoKit(address=0x41)
-kit3 = MockServoKit(address=0x42)
-kits = [kit1, kit2, kit3]
+USE_MOCK_HARDWARE = True
 
-server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server.bind(('0.0.0.0', 52183))# Replace 00000 with desired port number
+HOST_IP = '0.0.0.0'
+PORT = 52183
 
-print("UDP server up and listening")
+if USE_MOCK_HARDWARE:
+    print("Mock Running")
+    from mock_driver import MockServoKit as ServoKit
+else:
+    print("Real Running")
+    from adafruit_servokit import Servokit
 
-while True:
-    data, addr = server.recvfrom(4096)
-    print(f"Received message from {addr}:{data}")
-    angles = json.loads(data.decode('utf-8'))
+try:
+    from config import MOTOR_MAP
+except ImportError:
+    print("Map not found, using default mapping.")# I dkn what default mapping is
+    MOTOR_MAP = {}
 
-    for i, angle in enumerate(angles):
-        board_id = i//16
-        channel_id = i%16
+def init_kits():
+    print("Initializing 3 kits")
+    drivers = {}
+    try:
+        drivers[0] = ServoKit(channels = 16, address = 0x40)
+        drivers[1] = ServoKit(channels = 16, address = 0x41)
+        drivers[2] = ServoKit(channels = 16, address = 0x42)
+        print("Initialization complete")
+    except Exception as e:
+        print(f"Error initializing kits: {e}")
+        return None
+    return drivers
 
-        if board_id < 3:
-            kits[board_id].servo[channel_id].angle = angle
+def safety_clamp(angle):
+    return max(0, min(180,angle))
+
+def apply_movements(drivers, data_list):
+    for motor_id, target_angle in enumerate(data_list):
+        
+        if motor_id in MOTOR_MAP:
+            board_idx, channel = MOTOR_MAP[motor_id]
+        else:
+            board_idx = motor_id // 16
+            channel = motor_id % 16
+
+        safe_angle = safety_clamp(target_angle)
+
+        if board_idx in drivers:
+            try:
+                drivers[board_idx].servo[channel].angle = safe_angle
+            except Exception as e:
+                print(f"Error for board {board_idx} Ch {channel}:{e}")
+
+def main():
+    kits = init_kits()
+    if not kits:
+        return
+    
+    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server.bind((HOST_IP, PORT))
+
+    print(f"UDP server up and listening on {HOST_IP}:{PORT}")
+
+    try:
+        while True:
+            data, addr = server.recvfrom(4096)
+            print(f"Received message from {addr}")
+            try:
+                angles = json.loads(data.decode('utf-8'))
+                if len(angles) > 0:
+                    apply_movements(kits, angles)
+
+            except json.JSONDecodeError:
+                print("Received invalid JSON data")
+
+    except KeyboardInterrupt:
+        print("\n User interrupted the program.")
+
+    finally:
+        server.close()
+        print("Socket closed")
